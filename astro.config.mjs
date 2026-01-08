58ca7d9b94d109bb3b036a8f921bc798d02a2737
theprath1 <20ce158@charusat.edu.in>
2026-01-08 22:56:17 +0530

    updated site url in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://prthmptl.github.io',
+  site: 'https://prathamp.com',
   base: '/',
   vite: {
     plugins: [tailwindcss()]
