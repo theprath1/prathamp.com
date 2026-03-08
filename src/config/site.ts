30363f168b5d26085712836e81cb31c85ce70357
prthmptl <20ce158@charusat.edu.in>
2026-03-08 12:44:53 +0530

    updated social media handles

--- src/config/site.ts
+++ src/config/site.ts
@@ -7,8 +7,8 @@ export const siteConfig = {
     name: "Pratham Patel",
     email: "prathammpatel@outlook.com",
     twitter: "theprath",
-    github: "theprath1",
-    linkedin: "theprath",
+    github: "prthmptl",
+    linkedin: "prthmptl",
   },
   newsletter: {
     mailchimpUrl: "https://prathamp.us1.list-manage.com/subscribe/post?u=b7fe9580cf3e2438b2c195e8f&amp;id=438603be55&amp;f_id=00c7c4e1f0",
